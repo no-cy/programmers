c90c58530fb7a2277f445c836291f8e8fb1a451d
no-cy <63999547+no-cy@users.noreply.github.com>
2022-04-28 22:31:46 +0900

    Update 소수만들기.c

--- Level1/소수만들기.c
+++ Level1/소수만들기.c
@@ -6,7 +6,6 @@ int prime(int sum)
 {
     for(int i=2; i<sum; i++) // 1은 소수이므로 2부터 시작.
     {
-        
         if(sum % i == 0)  return false; // 한번이라도 나누어 떨어지면 소수가 아니므로 false.
     }
     
@@ -18,8 +17,6 @@ int solution(int nums[], size_t nums_len) {
     int answer = 0;
     int i = 0, j = 0, k = 0;
     int sum_num = 0;
-    int result = 0;
-    int flag = 0;
     
     for(i=0; i<nums_len; i++)
     {

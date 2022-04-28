ddb7baa150a1cdb10d50c0a490f1ae314b7aae19
no-cy <63999547+no-cy@users.noreply.github.com>
2022-04-28 22:29:27 +0900

    Create 소수만들기.c
    문제 설명
    주어진 숫자 중 3개의 수를 더했을 때 소수가 되는 경우의 개수를 구하려고 합니다. 숫자들이 들어있는 배열 nums가 매개변수로 주어질 때, nums에 있는 숫자들 중 서로 다른 3개를 골라 더했을 때 소수가 되는 경우의 개수를 return 하도록 solution 함수를 완성해주세요.
    
    제한사항
    nums에 들어있는 숫자의 개수는 3개 이상 50개 이하입니다.
    nums의 각 원소는 1 이상 1,000 이하의 자연수이며, 중복된 숫자가 들어있지 않습니다.

---
+++ Level1/소수만들기.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int prime(int sum)
+{
+    for(int i=2; i<sum; i++) // 1은 소수이므로 2부터 시작.
+    {
+        
+        if(sum % i == 0)  return false; // 한번이라도 나누어 떨어지면 소수가 아니므로 false.
+    }
+    
+    return true; // 떨어지는 수가 없으면 true.   
+}
+
+// nums_len은 배열 nums의 길이입니다.
+int solution(int nums[], size_t nums_len) {
+    int answer = 0;
+    int i = 0, j = 0, k = 0;
+    int sum_num = 0;
+    int result = 0;
+    int flag = 0;
+    
+    for(i=0; i<nums_len; i++)
+    {
+        for(j=i+1; j<nums_len; j++)
+        {
+            for(k=j+1; k<nums_len; k++)
+            {
+                sum_num = nums[i] + nums[j] + nums[k];
+                answer += prime(sum_num);
+            }
+        }
+    }
+    
+    return answer;
+}
